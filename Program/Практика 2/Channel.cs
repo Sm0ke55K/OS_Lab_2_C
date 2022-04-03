e83bb0a1ac5fe28a1a839938f8df94bb41b68bf8
Sm0ke55K <90284279+Sm0ke55K@users.noreply.github.com>
2022-04-03 21:03:29 +0300

    Add files via upload

---
+++ Program/Практика 2/Channel.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Channels;
+
+namespace ConsoleApplication1
+{
+    internal class Channel<T>
+    {
+        public ChannelWriter<string> Writer { get; internal set; }
+    }
+}
