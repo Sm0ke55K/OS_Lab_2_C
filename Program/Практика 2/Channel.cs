b9f94384642ebfd108d3840e69346271b886e6af
Губанов Матвей <90284279+Sm0ke55K@users.noreply.github.com>
2022-04-14 22:58:08 +0300

    Delete Channel.cs

--- Program/Практика 2/Channel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Channels;
-
-namespace ConsoleApplication1
-{
-    internal class Channel<T>
-    {
-        public ChannelWriter<string> Writer { get; internal set; }
-    }
-}
